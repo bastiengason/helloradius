6c3b0744b66b5cc3e0776657b7f867c95273788e
bastiengason <gasonbastien@gmail.com>
2018-09-12 20:43:35 +0700

    html structure

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'application#home'
+
+  # Pages routes
+  get '/about', to: 'pages#about'
+  get '/blog', to: 'pages#blog'
+  get '/contact', to: 'pages#contact'
+  get '/faq', to: 'pages#faq'
+  get '/jobs', to: 'pages#jobs'
+  get '/terms', to: 'pages#terms'
+
+  # Plans routes
+  get '/plan', to: 'plans#plan'
+  get '/plan/starter', to: 'plans#starter'
+  get '/plan/professional', to: 'plans#professional'
+  get '/plan/unlimited', to: 'plans#unlimited'
+end
