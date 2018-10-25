66d13cdfec970f0f14183c1262e308e01234e284
Bastien Gason <gasonbastien@gmail.com>
2018-10-25 17:54:59 +0700

    add tooltip but still not showing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,10 @@ $(document).on('click', 'a[href^="#"]', function (event) {
       scrollTop: $($.attr(this, 'href')).offset().top
   }, 500);
 });
+
+
+
+$( document ).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
+  $('.feature').tooltip('show')
+});
